d4ce80e715c2c9108a4bf746d22a0abfe443d290
Alexey Semenov <a.semenov@b2b-center.ru>
2023-06-17 02:45:38 +0500

    update state

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 type Owner int64
@@ -13,7 +12,37 @@ const (
 	Neutral       = 0
 )
 
-func initialize() [][]int {
+type EntityType string
+
+type Graph [][]int
+
+const (
+	Factory EntityType = "FACTORY"
+	Troop   EntityType = "TROOP"
+)
+
+type Node struct {
+	id         int
+	owner      Owner
+	garrison   int
+	production int
+}
+
+type TroopSquad struct {
+	owner       Owner
+	source      Node
+	destination Node
+	size        int
+	arrival     int
+}
+
+type GameState struct {
+	graph  Graph
+	nodes  []Node
+	troops []TroopSquad
+}
+
+func Initialize() Graph {
 	// factoryCount: the number of factories
 	var factoryCount int
 	fmt.Scan(&factoryCount)
@@ -22,8 +51,7 @@ func initialize() [][]int {
 	var linkCount int
 	fmt.Scan(&linkCount)
 
-	graph := make([][]int, factoryCount)
-
+	graph := make(Graph, factoryCount)
 	for i := range graph {
 		graph[i] = make([]int, factoryCount)
 	}
@@ -37,29 +65,39 @@ func initialize() [][]int {
 	return graph
 }
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+func (state *GameState) UpdateState() {
+	var entityCount int
+	fmt.Scan(&entityCount)
+	state.nodes = state.nodes[:0]
+	state.troops = state.troops[:0]
+
+	for i := 0; i < entityCount; i++ {
+		var entityId int
+		var entityType EntityType
+		var owner Owner
+		var arg2, arg3, arg4, arg5 int
+		fmt.Scan(&entityId, &entityType, &owner, &arg2, &arg3, &arg4, &arg5)
+
+		switch entityType {
+		case Factory:
+			node := Node{id: entityId, owner: owner, garrison: arg2, production: arg3}
+			state.nodes = append(state.nodes, node)
+		case Troop:
+			troop := TroopSquad{owner: owner, source: state.nodes[arg2], destination: state.nodes[arg3], size: arg4, arrival: arg5}
+			state.troops = append(state.troops, troop)
+		}
+	}
+}
 
 func main() {
-	graph := initialize()
-	for i := 0; i < len(graph); i++ {
-		fmt.Fprintln(os.Stderr, graph[i])
-	}
+	graph := Initialize()
+
+	var nodes []Node
+	var troops []TroopSquad
+	state := GameState{graph: graph, nodes: nodes, troops: troops}
 
 	for {
-		// entityCount: the number of entities (e.g. factories and troops)
-		var entityCount int
-		fmt.Scan(&entityCount)
-
-		for i := 0; i < entityCount; i++ {
-			var entityId int
-			var entityType string
-			var arg1, arg2, arg3, arg4, arg5 int
-			fmt.Scan(&entityId, &entityType, &arg1, &arg2, &arg3, &arg4, &arg5)
-		}
-
+		state.UpdateState()
 		// Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
 		fmt.Println("WAIT")
 	}
