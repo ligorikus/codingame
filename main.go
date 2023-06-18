0ba209b7640b2572bf4ef56dec245ee8d1be9247
Alexey Semenov <a.semenov@b2b-center.ru>
2023-06-18 22:26:36 +0500

    v1 attack done

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"time"
 )
+
+const MaxDistance = 20
+const Deep = MaxDistance + 1
 
 type Owner int64
 
@@ -35,7 +40,8 @@ type Node struct {
 	owner      Owner
 	garrison   int
 	production int
-	incoming   []TroopSquad
+	incoming   [Deep][]TroopSquad
+	properties NodeProperties
 }
 
 type TroopSquad struct {
@@ -48,7 +54,13 @@ type TroopSquad struct {
 
 type GameState struct {
 	graph Graph
-	nodes []Node
+	nodes []*Node
+}
+
+type NodeProperties struct {
+	balance [Deep]int
+	owner   [Deep]Owner
+	cost    []int
 }
 
 func (state *GameState) InitializeGraph() {
@@ -74,9 +86,9 @@ func (state *GameState) InitializeGraph() {
 }
 
 func (state *GameState) InitializeNodes() {
-	state.nodes = make([]Node, len(state.graph))
+	state.nodes = make([]*Node, len(state.graph))
 	for i := range state.graph {
-		state.nodes[i] = Node{id: i}
+		state.nodes[i] = &Node{id: i, properties: NodeProperties{cost: make([]int, len(state.graph))}}
 	}
 }
 
@@ -102,11 +114,11 @@ func (state *GameState) UpdateState() {
 }
 
 func (state *GameState) UpdateNode(nodeId int, input GameInput) {
-	node := &state.nodes[nodeId]
+	node := state.nodes[nodeId]
 	node.owner = input.owner
 	node.garrison = input.arg2
 	node.production = input.arg3
-	node.incoming = make([]TroopSquad, 0)
+	node.incoming = [Deep][]TroopSquad{}
 }
 
 func (state *GameState) InputTroop(input GameInput) {
@@ -117,8 +129,85 @@ func (state *GameState) InputTroop(input GameInput) {
 	troop.size = input.arg4
 	troop.arrival = input.arg5
 
-	nodeIncoming := state.nodes[troop.destination].incoming
-	state.nodes[troop.destination].incoming = append(nodeIncoming, troop)
+	nodeIncoming := state.nodes[troop.destination].incoming[troop.arrival]
+	state.nodes[troop.destination].incoming[troop.arrival] = append(nodeIncoming, troop)
+}
+
+func (node *Node) CalculateBalance() {
+	node.properties.balance[0] = node.garrison
+	node.properties.owner[0] = node.owner
+
+	for i := 1; i < Deep; i++ {
+		node.properties.owner[i] = node.properties.owner[i-1]
+		node.properties.balance[i] = node.properties.balance[i-1]
+
+		if node.properties.owner[i] != Neutral {
+			node.properties.balance[i] = node.properties.balance[i-1] + node.production
+		}
+
+		incomingBalance := 0
+		var incomingWinner Owner
+		for _, troop := range node.incoming[i] {
+			incomingBalance += troop.size * int(troop.owner)
+		}
+		if incomingBalance != 0 {
+			if incomingBalance > 0 {
+				incomingWinner = Player
+			} else {
+				incomingWinner = Enemy
+				incomingBalance *= -1
+			}
+
+			if node.properties.owner[i] == incomingWinner {
+				node.properties.balance[i] += incomingBalance
+			} else {
+				node.properties.balance[i] -= incomingBalance
+			}
+
+			if node.properties.balance[i] < 0 {
+				node.properties.balance[i] *= -1
+				node.properties.owner[i] = incomingWinner
+			}
+		}
+	}
+}
+
+func (node *Node) CalculateCost(state GameState) {
+	if node.owner != Player {
+		return
+	}
+	directions := state.graph[node.id]
+	for i := 0; i < len(directions); i++ {
+		if i == node.id {
+			continue
+		}
+
+		currentNode := state.nodes[i]
+		troopSize := currentNode.properties.balance[directions[i]]
+
+		var payback float64
+		if currentNode.production != 0 {
+			payback = float64(troopSize) / float64(currentNode.production)
+			node.properties.cost[i] = int(math.Ceil(payback)) + directions[i]
+		} else {
+			node.properties.cost[i] = 200
+		}
+	}
+}
+
+func (node *Node) SearchCheap() int {
+	min := 999
+	minCostNode := 0
+	for i := 0; i < len(node.properties.cost); i++ {
+		if i == node.id {
+			continue
+		}
+		if node.properties.cost[i] < min {
+			minCostNode = i
+			min = node.properties.cost[i]
+		}
+	}
+	return minCostNode
 }
 
 func main() {
@@ -127,9 +216,30 @@ func main() {
 	state.InitializeNodes()
 
 	for {
+		start := time.Now()
 		state.UpdateState()
-		fmt.Fprintln(os.Stderr, state.nodes[8].incoming)
+
+		for _, node := range state.nodes {
+			node.CalculateBalance()
+		}
+		for _, node := range state.nodes {
+			node.CalculateCost(state)
+		}
+
+		fmt.Print("WAIT")
+		for _, node := range state.nodes {
+			if node.owner == Player {
+				fmt.Print(";")
+				targetNodeId := node.SearchCheap()
+				distance := state.graph[node.id][targetNodeId]
+				size := state.nodes[targetNodeId].properties.balance[distance] + 1
+				fmt.Print("MOVE ", node.id, targetNodeId, size)
+			}
+		}
+		fmt.Println()
 		// Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
-		fmt.Println("WAIT")
+
+		duration := time.Since(start)
+		fmt.Fprintln(os.Stderr, duration)
 	}
 }
